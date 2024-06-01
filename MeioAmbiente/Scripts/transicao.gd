0a962cc978c377fadeac7379d34f2c7fad324bf1
Mateus Coelho <mateuscoelhosoporo@gmail.com>
2024-05-31 23:55:53 -0400

    configuração das teclas de andar

--- MeioAmbiente/Scripts/transicao.gd
+++ MeioAmbiente/Scripts/transicao.gd
@@ -11,6 +11,7 @@ func change_scene(path, delay = 2.5):
 	scene_transition.tween_property(color_rect, "threshold", 1.0, 0.5)
 	await scene_transition.finished
 	assert(get_tree().change_scene_to_file(path) == OK)
+	color_rect.visible = false
 	
 func show_new_scene():
 	var show_transition = get_tree().create_tween() 
